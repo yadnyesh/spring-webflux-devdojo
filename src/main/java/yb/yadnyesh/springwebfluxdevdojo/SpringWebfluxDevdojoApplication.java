25da6cf635714f421afa4d6df8825609fc3ab110
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-19 17:10:24 +0530

    First Commit

---
+++ src/main/java/yb/yadnyesh/springwebfluxdevdojo/SpringWebfluxDevdojoApplication.java
@@ -0,0 +1,13 @@
+package yb.yadnyesh.springwebfluxdevdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxDevdojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxDevdojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yb/yadnyesh/springwebfluxdevdojo/SpringWebfluxDevdojoApplicationTests.java
@@ -0,0 +1,13 @@
+package yb.yadnyesh.springwebfluxdevdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxDevdojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
