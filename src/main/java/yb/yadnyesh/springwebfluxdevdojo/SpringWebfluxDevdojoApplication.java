7a8c1ba093cf6adf188b81c44f5b76d322ab422d
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-07-27 09:49:43 +0530

    Refactor

--- src/main/java/yb/yadnyesh/springwebfluxdevdojo/SpringWebfluxDevdojoApplication.java
+++ src/main/java/yb/yadnyesh/springwebfluxdevdojo/SpringWebfluxDevdojoApplication.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringWebfluxDevdojoApplication {
-
-//	static {
-//		BlockHound.install();
-//	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebfluxDevdojoApplication.class, args);
 	}
