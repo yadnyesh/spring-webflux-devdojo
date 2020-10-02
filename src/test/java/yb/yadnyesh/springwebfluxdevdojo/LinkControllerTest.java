b479df546b52c50414e582fc7d53f4a2124638e8
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-10-02 12:09:56 +0530

    Basic Test working with Dummy Shortened link

--- src/test/java/yb/yadnyesh/springwebfluxdevdojo/LinkControllerTest.java
+++ src/test/java/yb/yadnyesh/springwebfluxdevdojo/LinkControllerTest.java
@@ -9,6 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @WebFluxTest (controllers = LinkController.class)
 public class LinkControllerTest {
@@ -24,6 +26,9 @@ public class LinkControllerTest {
                 .syncBody("{\"link\":\"https://spring.io\"}")
                 .exchange()
                 .expectStatus()
-                .is2xxSuccessful();
+                .is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$.shortenedLink")
+                .value(val -> assertThat(val).isEqualTo("http://localhost:8080/abcd1234"));
     }
 }
